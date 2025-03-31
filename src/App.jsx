d1a2898a22a6fdd915ff622755fcc1ac2b991116
David Catalán <david.catalan@cuidum.com>
2025-03-31 18:08:01 +0200

    V2.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+
 
 import{BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -11,7 +15,16 @@ function App() {
             <Route path="/">
               <Route index element={<Home />} />
               <Route path="login" element={<Login/>} />
-
+              <Route path="users">
+                <Route index element={<List />} />
+                <Route path=":userID" element={<Single />} />
+                <Route path="new" element={<New />} />
+              </Route>
+              <Route path="products">
+                <Route index element={<List />} />
+                <Route path=":productID" element={<Single />} />
+                <Route path="new" element={<New />} />
+              </Route>
             </Route>
           </Routes>
         </BrowserRouter>
