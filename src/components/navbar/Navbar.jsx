2f3115435dddbf40cedd9fc576ab379891b16d53
David Catalán <david.catalan@cuidum.com>
2025-04-01 11:46:26 +0200

    V2.3

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,49 @@
 import"./navbar.scss";
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
 
 const Navbar = () =>{
     return(
         <div className="navbar">
-            Navbar
+            <div className="wrapper">
+                <div className="search">
+                    <input type="text" placeholder="Search..."/>
+                    <SearchOutlinedIcon />
+                </div>
+                <div className="items">
+                    <div className="item">
+                        <LanguageOutlinedIcon />
+                        English
+                    </div>
+                    <div className="item">
+                        <DarkModeOutlinedIcon />
+                        
+                    </div>
+                    <div className="item">
+                        <FullscreenExitOutlinedIcon />
+                        
+                    </div>
+                    <div className="item">
+                        <NotificationsNoneOutlinedIcon />
+                        
+                    </div>
+                    <div className="item">
+                        <ChatBubbleOutlineOutlinedIcon />
+                        
+                    </div>
+                    <div className="item">
+                        <FormatListBulletedOutlinedIcon />
+                        
+                    </div>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,15 @@
 import "./home.scss";
 import Sidebar from "../../components/sidebar/sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 const Home = () =>{
     return(
         <div className="home">
-            <Sidebar></Sidebar>
+            <Sidebar />
+            <div className="homeContainer">
+                <Navbar />
+                home container
+            </div>
         </div>
     )
 }
