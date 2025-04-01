cbcd74e922baf9615e2d7357be45e60993625f85
David Catalán <david.catalan@cuidum.com>
2025-04-01 16:20:29 +0200

    V2.7

--- src/components/widget/Widgets.jsx
+++ src/components/widget/Widgets.jsx
@@ -53,7 +53,7 @@ const Widget = ({ type }) =>{
         <div className="widget">
             <div className="left">
                 <span className="title">{data.title}</span>
-                <span className="counter">{data.isMoney && "$"} {amount}</span>
+                <span className="counter">{amount}{data.isMoney && "€"}</span>
                 <span className="link">{data.link}</span>
             </div>
             <div className="right">
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import "./home.scss";
 import Sidebar from "../../components/sidebar/sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widgets";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
 
 const Home = () =>{
     return(
@@ -16,7 +18,8 @@ const Home = () =>{
                     <Widget type="balance"/>
                 </div>
                 <div className="charts">
-                    
+                    <Featured/>
+                    <Chart/>
                 </div>
             </div>
         </div>
