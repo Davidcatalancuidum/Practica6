fc5142067cd5ba5ad92bda62334e2616ffd4cf14
David Catalán <david.catalan@cuidum.com>
2025-04-02 17:44:41 +0200

    V3.6

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { DarkModeContextProvider } from "./context/darkModeContext";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <DarkModeContextProvider>
     <App />
-  </DarkModeContextProvider>
-);
+</DarkModeContextProvider>,
+)
