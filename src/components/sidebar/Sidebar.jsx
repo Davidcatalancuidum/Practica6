3c72eb061fd979f17f09bd5ba049e08512403d00
David Catalán <david.catalan@cuidum.com>
2025-04-02 17:13:11 +0200

    V3.5

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -12,13 +12,16 @@ import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import {Link} from "react-router-dom";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
 
 const Sidebar = () =>{
+    const { dispatch } = useContext(DarkModeContext);
     return(
         <div className="sidebar">
             <div className="top">
                 <Link to="/" style={{textDecoration: "none"}}>
-                <span className="logo">David</span>
+                    <span className="logo">David</span>
                 </Link>
             </div>
             <hr />
@@ -84,8 +87,8 @@ const Sidebar = () =>{
                 </ul>
             </div>
             <div className="bottom">
-                <div className="colorOption"></div>
-                <div className="colorOption"></div>
+                <div className="colorOption" onClick={() => dispatch({ type: "LIGHT" })}></div>
+                <div className="colorOption" onClick={() => dispatch({ type: "DARK" })}></div>
             </div>
         </div>
     );
